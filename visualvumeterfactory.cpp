72f5a64d49874482abbea1b2129f08c87dc6437d
Greedysky <greedysky@163.com>
2025-11-07 15:18:09 +0800

    feat: Support for qmmp 0.x Qt4

--- visualvumeterfactory.cpp
+++ visualvumeterfactory.cpp
@@ -39,3 +39,8 @@ QString VisualVUMeterFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(vumeter, VisualVUMeterFactory)
+#endif
